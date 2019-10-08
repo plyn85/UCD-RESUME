a574551b7383fe8c2b0d3f5119d5f880bbac590a
plyn85 <plyn99@gmail.com>
2019-10-08 19:09:58 +0100

    adding github information function

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,3 +1,23 @@
+//user.name will return the users public display name
+//href will provide a link to the user's public profile on GitHub.
+//gh-content is wheere the is where the content about the user is going to appear
+//gh-avatar is where there git hub avatar will appear
+function userInfomationHTML(user) {
+  return;
+  `<h2>${user.name}
+  <span>"small-name<'span>
+  (@<a href="${user.hml_url}"target="_blank">${user.login}</a>)
+  </span></h2>
+  <div class="gh-content>
+  <div class="gh-avatar>
+  <a href="${user.html_url}"target="blank">
+  <img src="${user.avatar_url}" width="80px height="80px" alt="${user.login}"/>
+  </a> <div>
+  <<br>Followers: ${user.followers}-folllowing${user.following}<br>Repos: ${user.pubic_repos}</p>
+  </div>
+  `;
+}
+
 //function to feth the git hub infomation here
 function fetchGitHubInformation(event) {
   //var added here for username value(what goes inside the box)
@@ -16,9 +36,19 @@ function fetchGitHubInformation(event) {
 
   //Promises
   //$when(we got a response from the GitHub API )
-  //.then (function to display in thr gh-user-data-div)
+  //.then (function to display in the gh-user-data-div)
   //unless we get an error!is whats happening below
-  //when method takes get jason function as its first argument
+  //when() method takes get jason function as its first argument
+  //when() thats done then() we want to display the github user data  it in gh-user-data-div(the box)
+  //the response passed in as agrument is the response that came back from our get json method
+  //then select user data div//(userInfomationHTML is a function that hasnt been created yet userdata(response) will be passed in
+  //an error function is then added//if errorResponse is not found then 'no infomation was found'
+  //which means if the git hub username cannot be found
+  // ' No Infomation was found for user ' will be displayed plus the incorrect github
+  // {username}that was wriiten as a value in the box
+  //we add the last else incase the error that comes back is not a 404 error
+  //then error plus the json response will be displayed
+
   $.when($.getJSON(`https://api.github.com/users/${username}`)).then(
     function(response) {
       var userData = response;
@@ -27,10 +57,10 @@ function fetchGitHubInformation(event) {
     function(errorResponse) {
       if (errorResponse.status === 404) {
         $("gh-user-data").html(
-          `<h2>No Infomation was found for ${username}</h2>`
+          `<h2>No Infomation was found for user ${username}</h2>`
         );
       } else {
-        console.log(error);
+        console.log(errorResponse);
         $("#gh-user-data").html(
           `<h2>Error${errorResponse.responseJSON.message}</h2>`
         );
